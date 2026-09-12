15ea8b9bd9fd472439802a8d8da7e9ccfe0467da
evilspoter69 <123252436+evilspotter@users.noreply.github.com>
2026-09-12 21:13:22 +0530

    Add files via upload

---
+++ assasin.py
@@ -0,0 +1,83 @@
+# a=20
+# b=10
+# a=a+b
+# print(a);
+
+# a=20 
+# b=5
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a%b)
+
+# print(25%4)
+
+# print(5**3)
+
+# a=50
+# b=30
+# print(a>b)
+
+# a=25
+# b=25
+# print(a==b)
+
+# print(100>=100)
+
+# age= int(input("enter your age: "))
+# if age>=18:
+#   print("ligal age")
+# else:
+#   print("pocso")
+
+# age= int(input("enter your age "))
+# print(age>=18 and age<=60)
+
+# maths=80
+# science=35
+# print(maths>=40 or science>=40)
+
+# x=True
+# print(not(x))
+
+# age= int(input("age cheppara"))
+# qualification= input("qualifiction")
+# if age>=18 and qualification=="degree":
+#     print("eligivle")
+# else:
+#     print("not eligible")
+
+# languages = ["Java", "Python", "C++", "SQL"]
+# print("Python" in languages )\
+
+# languages = ["Python", "Java", "C++"]
+# print("javasccript" not in languages)
+
+# name="Aparna"
+# print("a" in name)
+
+# a = [1, 2, 3]
+# b = a
+# print( a is b)
+
+# a = [1, 2, 3]
+# b = [1, 2, 3]
+# print(a is not b)
+
+# x=20 
+# x+=10
+# print(x)
+
+# x=10
+# x*=5 
+# print(x)
+
+# x=100
+# x+=20
+# print(x)
+# x-=10
+# print(x)
+# x*=2
+# print(x)
+# x/=2
+# print(x)
